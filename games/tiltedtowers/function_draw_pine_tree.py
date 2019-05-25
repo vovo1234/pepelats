184b1475d0228c88792f5dc2f0efa4f2d5305d4a
Basil Solovyev <basilsolovyev@neuromancer.local>
2019-05-25 13:22:07 -0600

    ignore .DS_Store files, and adding drawing trees

---
+++ games/tiltedtowers/function_draw_pine_tree.py
@@ -0,0 +1,36 @@
+import turtle, time
+
+def draw_pine_tree (size, start_x, start_y, speed, trunk_color, needles_color):
+    turn_number = 135
+    turtle.speed(speed)
+    turtle.penup()
+    turtle.goto(start_x, start_y)
+    turtle.pendown()
+    turtle.left(90)
+    turtle.color(trunk_color)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.color(needles_color)
+    turtle.begin_fill()
+    turtle.forward(size)
+    turtle.left(turn_number)
+    turtle.forward(size)
+    turtle.right(turn_number)
+    turtle.forward(size/2)
+    turtle.left(turn_number)
+    turtle.forward(size + size/10)
+    turtle.left(90)
+    turtle.forward(size + size/10)
+    turtle.left(turn_number)
+    turtle.forward(size/2)
+    turtle.right(turn_number)
+    turtle.forward(size)
+    turtle.left(turn_number)
+    turtle.forward(size)
+    turtle.end_fill()
+
+
+
+
+draw_pine_tree(30, 60, 60, 0, 'brown', 'green')
+time.sleep(3)
