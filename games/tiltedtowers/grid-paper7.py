42226fa2912f86373e43de1bf1cd9ba45e1b6475
Lex Lomanov <lexylom@gmail.com>
2019-06-01 16:43:56 -0600

    adding grid

---
+++ games/tiltedtowers/grid-paper7.py
@@ -0,0 +1,21 @@
+import turtle
+def grid(step, cellnumber):
+    x=step*cellnumber/2
+    y=step*cellnumber/2
+    for loop in range(cellnumber+1):
+        turtle.up()
+        turtle.setpos(-x, -y)
+        turtle.down()
+        turtle.setpos( x, -y)
+        y=y-step
+    x=step*cellnumber/2
+    y=step*cellnumber/2
+    for loop in range(cellnumber+1):
+        turtle.up()
+        turtle.setpos( x,y)
+        turtle.down()
+        turtle.setpos(x, -y)
+        x=x-step
+grid(20, 6)
+        
+        
