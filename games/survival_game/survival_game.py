b2b3352f26b377bfb3221531e4dd53f65e605924
Basil Solovyev <basilsolovyev@neuromancer.local>
2019-06-22 18:00:26 -0600

    survival game v9 major radiation update and inspecion key !!!READ INSTRUCTIONS!!!

--- games/survival_game/survival_game.py
+++ games/survival_game/survival_game.py
@@ -1,93 +1,100 @@
 import turtle
 import random
-import time as time
+import time
 
 wn = turtle.Screen()
 wn.bgcolor('Sea Green')
 wn.title('Survive')
 
-wn.addshape('player11.gif')
-wn.addshape('player21.gif')
-wn.addshape('player31.gif')
-wn.addshape('player41.gif')
+wn.addshape('images/player11.gif')
+wn.addshape('images/player21.gif')
+wn.addshape('images/player31.gif')
+wn.addshape('images/player41.gif')
 
-wn.addshape('player12.gif')
-wn.addshape('player22.gif')
-wn.addshape('player32.gif')
-wn.addshape('player42.gif')
+wn.addshape('images/player12.gif')
+wn.addshape('images/player22.gif')
+wn.addshape('images/player32.gif')
+wn.addshape('images/player42.gif')
 
-wn.addshape('player10.gif')
-wn.addshape('player20.gif')
-wn.addshape('player30.gif')
-wn.addshape('player40.gif')
+wn.addshape('images/player10.gif')
+wn.addshape('images/player20.gif')
+wn.addshape('images/player30.gif')
+wn.addshape('images/player40.gif')
 
-wn.addshape('player13.gif')
-wn.addshape('player23.gif')
-wn.addshape('player33.gif')
-wn.addshape('player43.gif')
+wn.addshape('images/player13.gif')
+wn.addshape('images/player23.gif')
+wn.addshape('images/player33.gif')
+wn.addshape('images/player43.gif')
 
-wn.addshape('stick.gif')
+wn.addshape('images/stick.gif')
 
-wn.addshape('rock.gif')
+wn.addshape('images/rock.gif')
 
-wn.addshape('pine_tree.gif')
+wn.addshape('images/pine_tree.gif')
 
-wn.addshape('berry_bush.gif')
+wn.addshape('images/berry_bush.gif')
 
-wn.addshape('pond.gif')
+wn.addshape('images/pond.gif')
 
-wn.addshape('zombie1.gif')
-wn.addshape('zombie2.gif')
-wn.addshape('zombie3.gif')
-wn.addshape('zombie4.gif')
+wn.addshape('images/zombie1.gif')
+wn.addshape('images/zombie2.gif')
+wn.addshape('images/zombie3.gif')
+wn.addshape('images/zombie4.gif')
 
-wn.addshape('campfire1.gif')
-wn.addshape('campfire2.gif')
+wn.addshape('images/campfire1.gif')
+wn.addshape('images/campfire2.gif')
 
 water = turtle.Turtle()
-water.shape('pond.gif')
+water.shape('images/pond.gif')
 water.turtlesize(3)
 water.penup()
 water.setpos(random.randint(-275, 275), random.randint(-275, 275))
 
 bush = turtle.Turtle()
-bush.shape('berry_bush.gif')
+bush.shape('images/berry_bush.gif')
 bush.turtlesize(3)
 bush.penup()
 bush.setpos(random.randint(-275, 275), random.randint(-275, 275))
 
 stick = turtle.Turtle()
-stick.shape('stick.gif')
+stick.shape('images/stick.gif')
 stick.turtlesize(2, 0.25)
 stick.penup()
 stick.setpos(random.randint(-275, 275), random.randint(-275, 275))
 
 rock = turtle.Turtle()
-rock.shape('rock.gif')
+rock.shape('images/rock.gif')
 rock.turtlesize(0.5)
 rock.penup()
 rock.setpos(random.randint(-275, 275), random.randint(-275, 275))
 
 tree = turtle.Turtle()
-tree.shape('pine_tree.gif')
+tree.shape('images/pine_tree.gif')
 tree.turtlesize(3)
 tree.penup()
 tree.setpos(random.randint(-275, 275), random.randint(-275, 275))
 
 fire = turtle.Turtle()
 fire.hideturtle()
-fire.shape('campfire1.gif')
+fire.shape('images/campfire1.gif')
 fire.penup()
 
 zombie = turtle.Turtle()
 zombie.hideturtle()
-zombie.shape('circle')
+zombie.shape('images/zombie1.gif')
 zombie.turtlesize(2)
 zombie.penup()
 zombie.setpos(random.randint(-275, 275), random.randint(-275, 275))
 
+rad_away = turtle.Turtle()
+rad_away.hideturtle()
+rad_away.shape('square')
+rad_away.turtlesize(1, 0.5)
+rad_away.penup()
+rad_away.setpos(random.randint(-275, 275), random.randint(-275, 275))
+
 player = turtle.Turtle()
-player.shape('player10.gif')
+player.shape('images/player10.gif')
 player.turtlesize(2)
 player.penup()
 
@@ -97,42 +104,42 @@ wasted_screen.hideturtle()
 
 def up():
     player.setpos(player.xcor(), player.ycor() + 5)
-    player.shape('player12.gif')
-    player.shape('player11.gif')
+    player.shape('images/player12.gif')
+    player.shape('images/player11.gif')
     if axe is True:
-        player.shape('player13.gif')
+        player.shape('images/player13.gif')
     else:
-        player.shape('player10.gif')
+        player.shape('images/player10.gif')
 
 
 def down():
     player.setpos(player.xcor(), player.ycor() - 5)
-    player.shape('player32.gif')
-    player.shape('player31.gif')
+    player.shape('images/player32.gif')
+    player.shape('images/player31.gif')
     if axe is True:
-        player.shape('player33.gif')
+        player.shape('images/player33.gif')
     else:
-        player.shape('player30.gif')
+        player.shape('images/player30.gif')
 
 
 def left():
     player.setpos(player.xcor() - 5, player.ycor())
-    player.shape('player22.gif')
-    player.shape('player21.gif')
+    player.shape('images/player22.gif')
+    player.shape('images/player21.gif')
     if axe is True:
-        player.shape('player43.gif')
+        player.shape('images/player43.gif')
     else:
-        player.shape('player20.gif')
+        player.shape('images/player20.gif')
 
 
 def right():
     player.setpos(player.xcor() + 5, player.ycor())
-    player.shape('player42.gif')
-    player.shape('player41.gif')
+    player.shape('images/player42.gif')
+    player.shape('images/player41.gif')
     if axe is True:
-        player.shape('player23.gif')
+        player.shape('images/player23.gif')
     else:
-        player.shape('player40.gif')
+        player.shape('images/player40.gif')
 
 
 bush_health = random.randint(1, 5)
@@ -142,10 +149,13 @@ stick_health = random.randint(1, 3)
 rocks = 0
 rock_health = random.randint(1, 2)
 heat = False
+rad_aways = 0
+rad_away_health = 1
 
 
 def action():
-    global berries, bush_health, wood, stick_health, rocks, rock_health, wood, axe, zombie_health
+    global berries, bush_health, wood, stick_health, rocks, rock_health, wood, axe, zombie_health, collect_rad_away,\
+        rad_aways, rad_away_health
     if player.distance(bush) < 50:
         berries += 1
         bush_health -= 1
@@ -159,6 +169,19 @@ def action():
         wood += 2
     if player.distance(zombie) < 40:
         zombie_health -= 1
+    if player.distance(rad_away) < 20 and collect_rad_away is True:
+        rad_aways += 1
+        rad_away_health -= 1
+
+
+collect_rad_away = False
+
+
+def inspect_action():
+    global collect_rad_away
+    if player.distance(rad_away) < 45:
+        collect_rad_away = True
+        rad_away.showturtle()
 
 
 berries = 0
@@ -192,6 +215,14 @@ def craft_axe():
         axe = True
 
 
+def use_rad_away():
+    global radiation, rad_aways, rad_away_health
+    if rad_aways > 0:
+        radiation -= 90
+        rad_aways -= 1
+        rad_away_health -= 1
+
+
 health = 10
 hydration = 50
 warmth = 50
@@ -200,15 +231,34 @@ game_time = 0
 zombie_attack = False
 start = time.time()
 alive = True
+radiation = 0
 
 
 def tick_update():
     global food_left, berries, health, hydration, warmth, bush_health, stick_health, rock_health,\
-        campfire_health, heat, game_time, zombie_attack, zombie_health, alive, rocks, wood
+        campfire_health, heat, game_time, zombie_attack, zombie_health, alive, rocks, wood, radiation,\
+        rad_away_health, rad_aways
     food_left -= 1
     hydration -= 1
     game_time += 1
-    fire.shape('campfire2.gif')
+    radiation += 1
+    fire.shape('images/campfire2.gif')
+
+    if rad_away_health <= 0:
+        rad_away.hideturtle()
+        rad_away.setpos(random.randint(-275, 275), random.randint(-275, 275))
+        rad_away_health = 1
+
+    if radiation <= 0:
+        radiation = 0
+
+    if radiation >= 70:
+        health -= 1
+        if radiation >= 100:
+            health -= 2
+
+    if radiation >= 100:
+        radiation = 100
 
     if hydration <= 0:
         hydration = 0
@@ -238,19 +288,19 @@ def tick_update():
         zombie.showturtle()
         if zombie.ycor() > player.ycor():
             zombie.setpos(zombie.xcor(), zombie.ycor() - 10)
-            zombie.shape('zombie3.gif')
+            zombie.shape('images/zombie3.gif')
 
         if zombie.ycor() < player.ycor():
             zombie.setpos(zombie.xcor(), zombie.ycor() + 10)
-            zombie.shape('zombie1.gif')
+            zombie.shape('images/zombie1.gif')
 
         if zombie.xcor() > player.xcor():
             zombie.setpos(zombie.xcor() - 10, zombie.ycor())
-            zombie.shape('zombie4.gif')
+            zombie.shape('images/zombie4.gif')
 
         if zombie.xcor() < player.xcor():
             zombie.setpos(zombie.xcor() + 10, zombie.ycor())
-            zombie.shape('zombie2.gif')
+            zombie.shape('images/zombie2.gif')
     else:
         zombie.hideturtle()
         zombie_attack = False
@@ -296,9 +346,9 @@ def tick_update():
     turtle.penup()
     turtle.setpos(-300, 270)
     turtle.write('food left: %s, berries: %s, health: %s, hydration: %s, warmth: %s, wood: %s, rocks: %s,'
-    ' campfire health: %s, time: %s'
+        ' campfire health: %s, time: %s, radiation: %s, rad-aways: %s'
         % (food_left, berries,  health, hydration, warmth, wood,
-        rocks, campfire_health, game_time), font=("Arial", 7, "normal"))
+        rocks, campfire_health, game_time, radiation, rad_aways), font=("Arial", 9, "normal"))
 
     if food_left >= 50:
         food_left = 50
@@ -334,7 +384,7 @@ def tick_update():
         rock.setpos(random.randint(-275, 275), random.randint(-275, 275))
         rock_health = random.randint(1, 5)
 
-    fire.shape('campfire1.gif')
+    fire.shape('images/campfire1.gif')
 
     if alive is True:
         turtle.ontimer(tick_update, 1000)
@@ -348,6 +398,8 @@ turtle.onkeypress(action, 'e')
 turtle.onkeypress(eat, '1')
 turtle.onkeypress(build_campfire, '2')
 turtle.onkeypress(craft_axe, '3')
+turtle.onkeypress(inspect_action, 'i')
+turtle.onkeypress(use_rad_away, '4')
 
 tick_update()
 
