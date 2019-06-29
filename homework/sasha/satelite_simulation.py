3304877e87ccbfbbd096cc6ab03eb24738df91ac
Alexander Smirnov <alexandersmir@outlook.com>
2019-06-29 16:26:26 -0600

    models a body in space orbiting a planet.

--- homework/sasha/satelite_simulation.py
+++ homework/sasha/satelite_simulation.py
@@ -1,29 +1,35 @@
 import turtle
+import math
 planet_radius = 6371000
-
-zoom = 2
+t = 0
+zoom = 25
 h = turtle.window_height()
 w = turtle.window_width()
 #coordinates
 x = 0
-y = 0
+y = planet_radius
 #velocity
-vx = 10
-vy = 10
+vx = 11000
+vy = 0
 #accelaration
 ax = 0
 ay = -10
 
 dt = 1/20
 
-#speed control yup lol(;
-time_scale = 100
+#speed control === yup lol(;
+time_scale = 10000
 
 turtle.setworldcoordinates(-planet_radius*zoom, -planet_radius*zoom*h/w,
                            planet_radius*zoom, planet_radius*zoom*h/w)
+orbiter = turtle.Turtle()
+orbiter.shape('circle')
+orbiter.color('red')
 
+text_writer = turtle.Turtle()
 
 turtle.speed(0)
+turtle.hideturtle()
 turtle.color('light sky blue')
 turtle.up()
 turtle.setpos(0, -planet_radius)
@@ -32,18 +38,34 @@ turtle.begin_fill()
 turtle.circle(planet_radius)
 turtle.end_fill()
 
+
 def do_step():
-    global x, y, vx, vy, ax, ay, dt
+    global x, y, vx, vy, ax, ay, dt, t
     dt_scaled = dt*time_scale
+    x = x + vx * dt_scaled
+    y = y + vy * dt_scaled
+    t = t + dt_scaled 
     G = 6.67408e-11
     planet_mass = 5.972e24
     r = math.sqrt(x*x + y*y)
     a = G*planet_mass/(r*r)
     ax = -x/r*a
     ay = -y/r*a
-    x = x + vx * dt_scaled
-    y = y + vy * dt_scaled
     vx = vx + ax * dt_scaled
     vy = vy + ay * dt_scaled
+
+def update():
+    global x, y, dt, orbiter
+    global vx, vy, t
+    do_step()
+    v = math.sqrt(vx*vx + vy*vy)
+    orbiter.setpos(x,y)
+    text_writer.undo()
+    text_writer.write(f'v = {v}\nt = {t}')
+    turtle.ontimer(update, (int(dt * 1000)))
+
+update()
+    
+
 
 turtle.mainloop()
